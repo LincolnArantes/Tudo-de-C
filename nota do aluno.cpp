d2b9eb38696b6864a3ae283462f19591eaad5a7a
Lincoln César dos Reis Arantes <44095737+LincolnArantes@users.noreply.github.com>
2023-03-07 00:23:04 -0300

    Add files via upload

---
+++ nota do aluno.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(){
+	
+	
+	int nota;
+	
+
+	printf("Digite a nota do aluno: \n");
+	
+	scanf("%d", &nota);
+	
+	if( nota >= 60){
+	
+	printf("APROVADO");
+	
+	
+} else {
+
+    printf("REPROVADO");	
+	
+
+	}	
+	
+	
+	
+	
+	
+	
+return 0;	
+	
+}
---
+++ numero igual.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int _numero = 10;
+
+    if (_numero == 10)
+    {
+        printf("O numero = 10");   
+    }
+    else
+    {
+        printf("O numero nao = 10");   
+    }
+
+
+return 0;
+
+}
