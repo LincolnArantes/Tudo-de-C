537fb0360f7c8b1f0a6297d7321d816cf083d433
Lincoln César dos Reis Arantes <44095737+LincolnArantes@users.noreply.github.com>
2023-03-07 00:20:37 -0300

    Add files via upload

---
+++ maior ou menor de idade.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+
+int main()
+{
+    int minha_idade = 15;
+    
+    
+
+    if(minha_idade > 17)
+    {
+      
+	    printf("Voce maior de idade.\n");
+    
+	}
+    else
+    {
+  
+        printf("Voce nao maior de idade.\n");
+  
+    }
+    
+    
+    return 0;
+    
+}
