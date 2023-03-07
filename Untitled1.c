873a171b1d6fa9ee06b5abe390d747601b14a9cd
Lincoln César dos Reis Arantes <44095737+LincolnArantes@users.noreply.github.com>
2023-03-07 00:33:34 -0300

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+
+int main() {
+   /* Meu primeiro programa em C */
+   printf("Hello, World! \n");
+   
+  
+
+
+
+int a, b;
+
+
+if (a == b){
+	
+	
+	printf("Igual");
+}
+
+
+
+return 0; 
+}
---
+++ trocando valores de variaveis.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(){
+	
+	
+   int a, b, c;
+   
+   
+   a = 10;
+   b = 20;
+   
+   c = a;
+   a = b;
+   b = c;
+   
+   printf("A variavel a agora tem %d\n", a);
+   printf("A variavel b agora tem %d\n", b);
+   
+   
+	
+	return 0;
+}
