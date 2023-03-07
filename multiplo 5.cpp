50e33f863761d2ac110b3e107af424016a0ce8cd
Lincoln César dos Reis Arantes <44095737+LincolnArantes@users.noreply.github.com>
2023-03-07 00:22:13 -0300

    Add files via upload

---
+++ multiplo 5.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n, resultado;
+	
+	
+	n = 10;
+	
+	
+resultado =	n / 5 * 5 + 5;
+
+
+printf("O multiplo de %d = %d", n, resultado);
+	
+	
+	
+	
+	
+	
+	return 0;
+}
