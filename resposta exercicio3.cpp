10eef1c755b6abfb17f2a34bbf748d5af4077ee3
Lincoln César dos Reis Arantes <44095737+LincolnArantes@users.noreply.github.com>
2023-03-07 00:26:41 -0300

    Add files via upload

---
+++ resposta exercicio3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+   /* Variavel */
+   int a = 3;
+   /* while loop */
+   while( a < 16 ) {
+      printf("%d\n", a);
+     a++;
+   }
+   return 0;
+}
+
---
+++ segundo if.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main () {
+
+   /* Local onde é definida a variavel */
+   int minha_variavel = 80;
+ 
+   /* verificação de condição */
+   if( minha_variavel == 10 ) {
+   	
+   	printf("O valor armazenado na variavel igual a 10\n");
+   	
+   	}else{
+	   
+   	
+   	 	printf("O valor armazenado na variavel nao igual a 10\n");
+   	 	
+   }
+   	
+   	printf("Por que o que esta armazenado na variavel = %d", minha_variavel);
+   	
+   	return 0;
+   	
+   }
