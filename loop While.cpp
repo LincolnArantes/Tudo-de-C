05b47329b69870732ce7ce2a53177893c1b91ccc
Lincoln César dos Reis Arantes <44095737+LincolnArantes@users.noreply.github.com>
2023-03-07 00:18:22 -0300

    Add files via upload

---
+++ loop While.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main () {
+
+   /* Variavel */
+   int a = 1;
+
+   /* while loop */
+   while( a < 11 ) {
+      printf("%d\n", a);
+     a++;
+   }
+ 
+   return 0;
+}
---
+++ loop(for) com condição.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main () {
+
+   int b;
+	
+   /* execução de loop */
+   for( b = 10; b < 20; b = b + 1 ){
+      printf("valor de b: %d\n", b);
+   }
+ 
+   return 0;
+}
