b5d30b5ea5639ef30a035d0621c00bcf5c99a005
Lincoln César dos Reis Arantes <44095737+LincolnArantes@users.noreply.github.com>
2023-03-07 00:28:04 -0300

    Add files via upload

---
+++ sobescrever a varial global.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+
+int gggg_1 = 20;
+ 
+int main () {
+
+
+  int gggg_1 = 10;
+ 
+  printf ("O valor de gggg_1 = %d\n",  gggg_1);
+ 
+  return 0;
+}
---
+++ soma com float.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(){
+	
+	float a, b, c;
+
+
+   a = 10.10;
+   b=55.10;
+   
+   c= a + b;
+   
+   printf("A soma =R$ %.2f", c);	
+	
+	
+	
+	
+	return 0;
+}
