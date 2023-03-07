18e477407aa0960d3859aced9a49495229c4b6e3
Lincoln César dos Reis Arantes <44095737+LincolnArantes@users.noreply.github.com>
2023-03-07 00:04:16 -0300

    Add files via upload

---
+++ correção_do_exercicio5.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main () {
+
+   /* local onde as variaveis são definidas */
+   int memoria = 10;
+ 
+   /* Verificão da condição */
+	
+
+   
+   
+   /* O valor abaixo sera mostrado de qualquer forma pois o mesmo não esta dentro da condição */
+   
+   printf("O valor da variavel memoria = %d\n", memoria);
+ 
+   return 0;
+}
